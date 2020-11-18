9b45de54dcbcdcae0a93937b59f8ee48d323517f
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-11-18 22:05:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -133,6 +133,7 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+   console.log(DODGER.style.left);
    var left=DODGER.style.left;
 
    function step() {
