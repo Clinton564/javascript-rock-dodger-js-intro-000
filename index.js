45e36aec28cec3b2a18b1371a0fb81282c42c35d
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-11-18 21:58:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ function moveDodger(e) {
    * And be sure to use the functions declared below!
    */
    if(e.which === LEFT_ARROW){
-     console.log(e);
+     console.log(e.which);
      moveDodgerLeft()
    }
 }
