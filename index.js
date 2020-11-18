d222e6496d74da73d01fa9224c2c9bb4fb7099f3
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-11-18 23:18:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -180,7 +180,7 @@ function moveDodgerRight() {
    var left=positionToInteger(DODGER.style.left);
 
    function step() {
-     DODGER.style.left = `${left += 1}px`
+     DODGER.style.left = `${left += 4}px`
 
      if (left < 360) {
        window.requestAnimationFrame(step)
